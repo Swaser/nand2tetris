e478f76aa024d1186e5dbb6ca06b055f98eb4493
Thomas Chassaing <thomas.chassaing@gmail.com>
2022-08-09 06:30:30 +0200

    Implement p04/Fill.asm

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,41 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+	// start with 24576-1 in R1
+	@24575
+	D=A
+	@R1
+	M=D
+(LOOP)
+	// R1 = R1 + 1
+	@R1
+	M=M+1
+	// if (R1 < 24576) goto KEYBOARD
+	D=M
+	@24576
+	D=D-A
+	@KEYBOARD
+	D;JLT
+	// R1 = SCREEN
+	@SCREEN
+	D=A
+	@R1
+	M=D
+(KEYBOARD)
+	// read keyboard; if nothing pressed whiten the screen
+	@KBD
+	D=M
+	@BLACKEN
+	D;JNE
+(WHITEN)
+	@R1
+	A=M
+	M=0
+	@LOOP
+	0;JEQ
+(BLACKEN)
+	@R1
+	A=M
+	M=-1
+	@LOOP
+	0;JEQ
