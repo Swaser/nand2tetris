b209afe74d2f8d032fdc32bcd278df60ad109a9e
Thomas Chassaing <Thomas.Chassaing@gmail.com>
2023-05-03 20:33:09 +0200

    Start work on Antlr4 version of Jack compiler

---
+++ Jack3/src/main/java/ch/chassaing/jack/ShortToUnicodeString.java
@@ -0,0 +1,27 @@
+package ch.chassaing.jack;
+
+import ch.chassaing.jack.arrayinit.ArrayInitBaseListener;
+import ch.chassaing.jack.arrayinit.ArrayInitParser;
+
+public class ShortToUnicodeString
+        extends ArrayInitBaseListener
+{
+    @Override
+    public void enterInit(ArrayInitParser.InitContext ctx)
+    {
+        System.out.print('"');
+    }
+
+    @Override
+    public void exitInit(ArrayInitParser.InitContext ctx)
+    {
+        System.out.print('"');
+    }
+
+    @Override
+    public void enterValue(ArrayInitParser.ValueContext ctx)
+    {
+        Integer value = Integer.valueOf(ctx.INT().getText());
+        System.out.printf("\\u%04x", value);
+    }
+}
