32dbd07d12102a956c99ba201854c55497539cee
Thomas Chassaing <Thomas.Chassaing@gmail.com>
2023-05-03 20:33:09 +0200

    Start work on Antlr4 version of Jack compiler

---
+++ Jack3/src/test/java/ch/chassaing/jack/ArrayInitTest.java
@@ -0,0 +1,20 @@
+package ch.chassaing.jack;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ArrayInitTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue(true);
+    }
+}
